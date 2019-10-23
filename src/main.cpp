34eba4027aa5c54ef0e5a22613ff7b9c23591adc
Yueyang123 <1700695611@qq.com>
2019-10-24 00:44:41 +0800

    10.24

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,11 @@
 
 #include "Random.h"		//随机算法头文件
 #include "common.h"
+ #include "polynomials.h"
 #include <iostream>			//输入输出流头文件			
 #include <time.h>			//时间头文件
 #include <complex>
 #include "Matrix.h"
-
-
 
 using namespace std;		//名字空间
 
